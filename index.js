1c0d0f96f6fbc4b1605fa7f7ca9dbeed2c65d628
Ivan Wills <ivan.wills@gmail.com>
2016-09-08 16:44:44 +1000

    Trying to make errors better

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const PLUGIN_NAME = 'gulp-indent-checker';
 
 module.exports = function (options) {
 	var stream = through.obj(function (file, enc, callback) {
+		try {
+			indent.assertIndent(file.contents + '', options);
+		} catch (e) {
+			console.log(e);
+			throw new PluginError(PLUGIN_NAME, file.path + ': ' + e);
+		}
 		callback();
 	});
 
