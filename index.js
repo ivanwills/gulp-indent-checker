686149ea1d456e31828664f6b627578c20242010
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-09-21 16:38:35 +1000

    Added option to only warn on errors and option to only die at the end of processing all files

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ var through     = require('through2'),
 const PLUGIN_NAME = 'gulp-indent-checker';
 
 module.exports = function (options) {
+	var errors = 0;
 	var stream = through.obj(function (file, enc, callback) {
 		try {
 			indent.assertIndent(file.contents + '', options);
 		} catch (e) {
-			console.log(e);
-			throw new PluginError(PLUGIN_NAME, file.path + ': ' + e);
+			if (options.warn || options.throwAtEnd) {
+				console.warn(file.path + ': ' + e);
+				errors++;
+			}
+			else {
+				console.log(e);
+				throw new PluginError(PLUGIN_NAME, file.path + ': ' + e);
+			}
 		}
 		callback();
+	})
+	.on('end', function () {
+		if (options.throwAtEnd && errors) {
+			var msg = errors > 1 ? 'There were ' + errors + ' files with errors!' :
+				'There was one file with an error!';
+			throw new PluginError(PLUGIN_NAME, msg);
+		}
 	});
 
 	return stream;
