4aff7eccaaeb9edddadd4e5477aba46222eef54d
Ivan Wills <ivan.wills@gmail.com>
2016-10-20 19:56:54 +1100

    adding reportors

--- index.js
+++ index.js
@@ -8,29 +8,51 @@ var through     = require('through2'),
 const PLUGIN_NAME = 'gulp-indent-checker';
 
 module.exports = function (options) {
-	var errors = 0;
 	var stream = through.obj(function (file, enc, callback) {
+		console.log(file.path);
 		try {
 			indent.assertIndent(file.contents + '', options);
 		} catch (e) {
-			if (options.warn || options.throwAtEnd) {
-				console.warn(file.path + ': ' + e);
-				errors++;
-			}
-			else {
-				console.log(e);
-				throw new PluginError(PLUGIN_NAME, file.path + ': ' + e);
-			}
+			console.log('here');
+			file.frror = e;
+			console.log(file.path, e);
 		}
-		callback();
+		try {
+			console.log(enc);
+			console.log(' ');
+		callback(file);
+		} catch (e) { console.error(e); }
+		console.info('???');
+	});
+
+	return stream;
+};
+
+module.exports.simpleReporter = function (options) {
+	var stream = through.obj(function (file, enc, callback) {
+		console.log('simple');
+		if (file.frror) {
+			console.warn(file.path + ': ' + file.frror);
+		}
+		callback(file);
+	});
+	return stream;
+};
+
+module.exports.errorReporter = function (options) {
+	var errors = 0;
+	var stream = through.obj(function (file, enc, callback) {
+		if (file.frror) {
+			errors++;
+		}
+		callback(file);
 	})
 	.on('end', function () {
-		if (options.throwAtEnd && errors) {
+		if (errors) {
 			var msg = errors > 1 ? 'There were ' + errors + ' files with errors!' :
 				'There was one file with an error!';
 			throw new PluginError(PLUGIN_NAME, msg);
 		}
 	});
-
 	return stream;
 };
