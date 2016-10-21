35fe3db908a103c55877b4a0d980dde452c425b7
Ivan Wills <ivan.wills@gmail.com>
2016-10-21 20:23:52 +1100

    Using new reporters

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ var gulp   = require('gulp'),
 	indent = require('./index');
 
 gulp.task('bad', function () {
-	return gulp.src('bad-file.txt')
+	return gulp.src(['*.js', 'bad-file.txt'])
 		.pipe(indent({}))
-		.pipe(indent.simpleReporter())
-		.pipe(indent.errorReporter());
+		.pipe(indent.reporter())
+		.pipe(indent.failOnError());
 });
 
 gulp.task('default', function () {
 	return gulp.src('*.js')
 		.pipe(indent({}))
-		.pipe(indent.simpleReporter())
-		.pipe(indent.errorReporter());
+		.pipe(indent.reporter())
+		.pipe(indent.failOnError());
 });
